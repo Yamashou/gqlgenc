4c48f3d0f7084782625b34d2390fcd52967fe82b
Yamashou <1230124fw@gmail.com>
2020-06-29 22:57:45 +0900

    add example of github api

---
+++ example/github/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/Yamashou/gqlgenc/client"
+	"github.com/Yamashou/gqlgenc/example/github/gen"
+)
+
+func main() {
+	// This example only read public repository. You don't need to select scopes.
+	token := os.Getenv("GITHUB_TOKEN")
+	authHeader := func(req *http.Request) {
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+	}
+
+	ctx := context.Background()
+
+	githubClient := &gen.Client{
+		Client: client.NewClient(http.DefaultClient, "https://api.github.com/graphql", authHeader),
+	}
+	getUser, err := githubClient.GetUser(ctx)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println(*getUser.Viewer.Name, getUser.Viewer.Repositories.Nodes[0].Name)
+	for _, repository := range getUser.Viewer.Repositories.Nodes {
+		fmt.Println(repository.Name)
+		for _, language := range repository.Languages.Nodes {
+			fmt.Println(language.Name)
+		}
+	}
+}
