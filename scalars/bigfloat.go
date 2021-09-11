7be06a0f8d413023470eee3f68466fe87fb78ddf
Conan Li <conan@triplemint.com>
2021-09-10 20:33:46 -0400

    handle various types

--- scalars/bigfloat.go
+++ scalars/bigfloat.go
@@ -3,47 +3,39 @@ package scalars
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"strconv"
 )
 
 type BigFloat float64
 
-func (f BigFloat) MarshalGQL(w io.Writer) {
-	_, err := w.Write([]byte(fmt.Sprintf("%f", f)))
-	if err != nil {
-		panic(err)
-	}
-}
-
-func (f *BigFloat) UnmarshalGQL(v interface{}) error {
-	stringValue, ok := v.(string)
-
-	if ok {
-		return fmt.Errorf("BigFloat must be a string")
-	}
-
-	floatValue, err := strconv.ParseFloat(stringValue, 64)
-	if err == nil {
-		*f = BigFloat(floatValue)
-
-		return nil
-	}
-
-	return err
-}
-
 func (f *BigFloat) UnmarshalJSON(b []byte) error {
-	var s string
-	if err := json.Unmarshal(b, &s); err != nil {
+	var v interface{}
+	if err := json.Unmarshal(b, &v); err != nil {
 		return err
 	}
 
-	floatValue, err := strconv.ParseFloat(s, 64)
-	if err == nil {
+	switch v := v.(type) {
+	case string:
+		floatValue, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			return nil
+		}
 		*f = BigFloat(floatValue)
 
-		return nil
+	case int:
+		*f = BigFloat(v)
+	case int64:
+		*f = BigFloat(v)
+	case float64:
+		*f = BigFloat(v)
+	case json.Number:
+		floatValue, err := strconv.ParseFloat(string(v), 64)
+		if err != nil {
+			return nil
+		}
+		*f = BigFloat(floatValue)
+	default:
+		return fmt.Errorf("%T is not a big float", v)
 	}
 
 	return nil
