8d6f240ca7324e214a3fc8ab4e2c41dc6478c21f
Manuel Laflamme <manuel.laflamme@gmail.com>
2024-04-23 16:48:01 -0400

    Rename struct_fields_always_pointers config tag to structFieldsAlwaysPointers

--- config/generate_config.go
+++ config/generate_config.go
@@ -14,7 +14,7 @@ type GenerateConfig struct {
 	// Deprecated: not working because v1 is deleted. Must use ClientV2
 	// if true, used client v2 in generate code
 	ClientV2                   bool  `yaml:"clientV2,omitempty"`
-	StructFieldsAlwaysPointers *bool `yaml:"struct_fields_always_pointers,omitempty"`
+	StructFieldsAlwaysPointers *bool `yaml:"structFieldsAlwaysPointers,omitempty"`
 }
 
 func (c *GenerateConfig) ShouldGenerateClient() bool {
