a3309a5ed73861cff849733a711907b4b44ae88e
sonatard <sonatard12@gmail.com>
2025-04-29 22:32:23 +0900

    Rename

--- clientgenv2/template_source.go
+++ clientgenv2/template_source.go
@@ -7,42 +7,38 @@ import (
 	"go/types"
 )
 
-// GetterFunc returns a function that generates getter methods for types.
-// targetPkgPath specifies the target package path and omits package qualifiers for types belonging to the same package.
-func GetterFunc() func(types.Type) string {
-	return func(t types.Type) string {
-		var namedType *types.Named
-		pointerType, ok := t.(*types.Pointer)
-		if ok {
-			namedType = pointerType.Elem().(*types.Named)
-		} else {
-			// FragmentのときはPointerではない
-			namedType = t.(*types.Named)
-		}
-		st := namedType.Underlying().(*types.Struct)
-
-		// typeName
-		typeName := toString(namedType)
-
-		var buf bytes.Buffer
-		fmt.Fprintf(&buf, "type %s %s\n", typeName, toString(st))
-		for i := range st.NumFields() {
-			field := st.Field(i)
-			fieldName := field.Name()
-			if fieldName == "" {
-				// fieldが埋め込みの時は、Getterは不要
-				continue
-			}
-
-			fieldTypeName := toString(field.Type())
-			fmt.Fprintf(&buf, "func (t *%s) Get%s() %s {\n", typeName, fieldName, fieldTypeName)
-			fmt.Fprintf(&buf, "\tif t == nil {\n\t\tt = &%s{}\n\t}\n", typeName)
-
-			fmt.Fprintf(&buf, "\treturn t.%s\n}\n", fieldName)
-		}
-
-		return buf.String()
+func TypeGenFunc(t types.Type) string {
+	var namedType *types.Named
+	pointerType, ok := t.(*types.Pointer)
+	if ok {
+		namedType = pointerType.Elem().(*types.Named)
+	} else {
+		// FragmentのときはPointerではない
+		namedType = t.(*types.Named)
 	}
+	st := namedType.Underlying().(*types.Struct)
+
+	// typeName
+	typeName := toString(namedType)
+
+	var buf bytes.Buffer
+	fmt.Fprintf(&buf, "type %s %s\n", typeName, toString(st))
+	for i := range st.NumFields() {
+		field := st.Field(i)
+		fieldName := field.Name()
+		if fieldName == "" {
+			// fieldが埋め込みの時は、Getterは不要
+			continue
+		}
+
+		fieldTypeName := toString(field.Type())
+		fmt.Fprintf(&buf, "func (t *%s) Get%s() %s {\n", typeName, fieldName, fieldTypeName)
+		fmt.Fprintf(&buf, "\tif t == nil {\n\t\tt = &%s{}\n\t}\n", typeName)
+
+		fmt.Fprintf(&buf, "\treturn t.%s\n}\n", fieldName)
+	}
+
+	return buf.String()
 }
 
 func toString(p types.Type) string {
